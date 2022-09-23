ee300cb3ea2c45570bad479c6d169ce15765b15d
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-09-23 10:41:17 +0530

    Create sol.cpp

---
+++ School/Print elements of an Array/sol.cpp
@@ -0,0 +1,10 @@
+class Solution{
+public:
+    //Just print the space seperated array elements
+	void printArray(int arr[], int n) {
+	    for(int el=0; el<n ;el++)
+	    {
+	        cout<<arr[el]<<" ";
+	    }
+	}
+};
