572c800a3d489845392554b2108dcb5f0ec16244
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-06 12:34:13 +0530

    https://practice.geeksforgeeks.org/problems/first-element-to-occur-k-times5150/1?page=1&curated[]=1&sortBy=submissions
    class Solution{
        public:
        int firstElementKTime(int a[], int n, int k)
        {
        // creating a map and storing the count
        unordered_map <int,int> mp;
        for(int i=0; i<n; i++){
            mp[a[i]]++;
            if(mp[a[i]] == k){
                return a[i];
            }
        }
        return -1;
        }
    };

---
+++ Basic/First Element to Occur K times/sol.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+    int firstElementKTime(int a[], int n, int k)
+    {
+    // creating a map and storing the count
+    unordered_map <int,int> mp;
+    for(int i=0; i<n; i++){
+        mp[a[i]]++;
+        if(mp[a[i]] == k){
+            return a[i];
+        }
+    }
+    return -1;
+    }
+};
