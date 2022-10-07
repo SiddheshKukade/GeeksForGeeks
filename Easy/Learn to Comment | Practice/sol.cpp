3bdd15ae171d38dfbffdc34a14c6731459e131e3
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-07 19:35:51 +0530

    Create sol.cpp

---
+++ Easy/Learn to Comment | Practice/sol.cpp
@@ -0,0 +1,9 @@
+//Comment the part that outputs b
+void comment(int a,int b,int c)
+{
+    printf("%d \n", a);
+    // printf("%d \n", b);
+    printf("%d \n", c);
+    //this is my good commnet
+    /* this is nater*/
+}
