2f3fcd68d9e252c8f9f21fe8cc6fbeb7909b78c8
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-07 18:52:25 +0530

    Update sol.cpp

--- Basic/Sum of first n terms/sol.cpp
+++ Basic/Sum of first n terms/sol.cpp
@@ -1,10 +1,15 @@
+
 class Solution {
   public:
-    long long sumOfSeries(long long N) {
-        long long sum =0;
-      for(int i=1 ; i<=N ;i++){
-       sum += i*i*i;   
-      }
+    long long sumOfSeries(long long n) {
+        long long    sum =0;
+        //using the for loop causes some errors of precision so we are using a already made
+        // formula for calculatin the sumOfSeries by cube
+    //   for(int i=1 ; i<=N ;i++){
+    //   sum +=pow(i , 3);   
+    //   }
+      sum = (n*(n+1) /2 );
+      sum *=sum;
       return sum;
     }
 };
