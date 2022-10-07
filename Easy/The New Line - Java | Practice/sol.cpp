bb8aadcd62f60fe024ddd5243754cdbf5510a49c
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-07 19:33:40 +0530

    Create sol.cpp

---
+++ Easy/The New Line - Java | Practice/sol.cpp
@@ -0,0 +1,11 @@
+class Geeks{
+    
+    // Function to print each word in single line
+    static void printIndividualLine(){
+     System.out.println("Geeks");
+          System.out.println("for");
+               System.out.println("Geeks");
+        
+    }
+    
+}
