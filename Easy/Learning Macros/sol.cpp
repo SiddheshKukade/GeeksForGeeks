5d09c71dfa6664c2b244c9893aa58bc85525bded
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-08 07:53:26 +0530

    Create sol.cpp

---
+++ Easy/Learning Macros/sol.cpp
@@ -0,0 +1,20 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#define MULTI(a,b) (a*b) //The multiplication macro function.
+/*Whenever MULTI(a,b) is encountered, it is replaced by (a*b) during the preprocessing stage*/
+using namespace std;
+
+void macros(int a,int b);
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+void macros(int a,int b)
+{
+    //See the macro defined at the top of the code. Use that macro to find a*b
+    //Use only macro to complete this task
+    
+    cout<<MULTI(a,b)<<endl;
+}
