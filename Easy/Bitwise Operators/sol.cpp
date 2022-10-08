397e6ce823dd7ede4a5810bd1b0fbb6b6bf0ef90
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-08 08:01:47 +0530

    Create sol.cpp
    void bitWiseOperation(int a, int b, int c) {
    int d ,e ,f,g,h;
    d = a^a;
    e = c^b;
    f= a&b;
    g= c| (a^a);
    h = ~e;
    cout<<d<<endl;
    cout<<e<<endl;
    cout<<f<<endl;
    cout<<g<<endl;
    cout<<h<<endl;
    }

---
+++ Easy/Bitwise Operators/sol.cpp
@@ -0,0 +1,13 @@
+void bitWiseOperation(int a, int b, int c) {
+int d ,e ,f,g,h;
+d = a^a;
+e = c^b;
+f= a&b;
+g= c| (a^a);
+h = ~e;
+cout<<d<<endl;
+cout<<e<<endl;
+cout<<f<<endl;
+cout<<g<<endl;
+cout<<h<<endl;
+}
