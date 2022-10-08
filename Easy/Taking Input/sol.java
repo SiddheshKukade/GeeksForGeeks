4e2fb5d3958e3023446c4fbb6e1e6576cb4776e6
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-08 07:25:37 +0530

    Create sol.java

---
+++ Easy/Taking Input/sol.java
@@ -0,0 +1,29 @@
+
+class Geeks{
+    
+    // Function to take input using Scanner class
+    static void IOFunction(){
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+        int num = (sc.nextInt());
+		 System.out.println(num);
+		 
+		   float nm = sc.nextFloat();
+		 System.out.println(nm);
+		 
+		    long  lg = sc.nextLong();
+		 System.out.println(lg);
+		 
+		  num = (sc.nextInt());
+		 System.out.println(num);
+		     sc.nextLine();
+		 String um = sc.nextLine();
+		 System.out.println(um);
+		    
+		}
+		
+    }
+    
+}
