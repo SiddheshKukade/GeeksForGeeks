405ca1b6bbb2318fd620b98142dda49b25de54ae
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-08 07:49:30 +0530

    Create sol.java

---
+++ Easy/nextLine and Next/sol.java
@@ -0,0 +1,27 @@
+
+//User function Template for Java
+
+//Complete the function
+//Take input for a and s 
+class Geeks {
+	static void getInput () {
+		
+	    Scanner sc = new Scanner(System.in);
+	    int t=sc.nextInt(); //Taking the number of testcases
+	    while(t-->0)
+	    {
+    		 int a = 0; 
+    	    String s ="";
+    	    
+            a= sc.nextInt();
+            sc.nextLine();
+            s = sc.nextLine();
+    
+    		System.out.println(a);
+    		System.out.println(s);
+    		
+	    }
+	
+	}
+}
+
