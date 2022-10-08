4424aa157c3cf0a18e8993aefe1717fe5d953a2e
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-08 07:44:11 +0530

    Create sol.cpp

---
+++ Easy/Taking Input/sol.cpp
@@ -0,0 +1,10 @@
+
+ void inputData()
+{
+       int a;
+       string b;
+       cin>>a;
+       cin>>b;
+       cout<<a<<" "<<b<<endl;
+      //Use cin to take input and cout a and b with a space between them. Also use an endl after output
+}
