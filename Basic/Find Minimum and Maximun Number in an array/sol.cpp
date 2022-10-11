69b9e1188c8d4ece16738d5420af1d9e20f22651
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-11 19:31:33 +0530

    https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1?page=1&curated[]=8&sortBy=submissions
    //{ Driver Code Starts
    #include <bits/stdc++.h>
    using namespace std;
    #define ll long long
    
    pair<long long, long long> getMinMax(long long a[], int n) ;
    
    int main() {
        int t;
        cin >> t;
        while (t--) {
            int n;
            cin >> n;
            ll a[n];
            for (int i = 0; i < n; i++) cin >> a[i];
    
            pair<ll, ll> pp = getMinMax(a, n);
    
            cout << pp.first << " " << pp.second << endl;
        }
        return 0;
    }
    // } Driver Code Ends
    
    
    pair<long long, long long> getMinMax(long long a[], int n) {
       pair<long long  , long long > pp ;
       pp.first = a[0];
       pp.second =a[0];
       for(int i=0 ;i<n;i++){
           if(pp.second < a[i]){ // max
               pp.second=a[i];
           }else if(pp.first > a[i]){ //min
               pp.first = a[i];
           }
       }
       return pp;
    }

---
+++ Basic/Find Minimum and Maximun Number in an array/sol.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+   pair<long long  , long long > pp ;
+   pp.first = a[0];
+   pp.second =a[0];
+   for(int i=0 ;i<n;i++){
+       if(pp.second < a[i]){ // max
+           pp.second=a[i];
+       }else if(pp.first > a[i]){ //min
+           pp.first = a[i];
+       }
+   }
+   return pp;
+}
