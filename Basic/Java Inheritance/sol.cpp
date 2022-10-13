7d41c64a9c447560cc1057cc046c2e0c930fbdaf
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-13 09:32:33 +0530

    Create sol.cpp

---
+++ Basic/Java Inheritance/sol.cpp
@@ -0,0 +1,13 @@
+class cls2 extends cls1
+{
+    void mul(int p,int q)
+    {
+        System.out.println(p*q);
+    }
+    void task(int p,int q)
+    {
+        // add(p,q);
+        // mul(p,q);
+        System.out.println(p*p+q*q);
+    }
+}
