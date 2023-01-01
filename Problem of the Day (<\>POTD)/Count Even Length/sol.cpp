d08b688497a2a5aac7011a33b2b10c8475d63210
Siddhesh Bhupendra Kuakde <contact@siddheshkukade.com>
2023-01-02 00:00:32 +0530

    Update sol.cpp

--- Problem of the Day (<\>POTD)/Count Even Length/sol.cpp
+++ Problem of the Day (<\>POTD)/Count Even Length/sol.cpp
@@ -1,3 +1,9 @@
+// This code is defining a function called inv that takes a single argument r of type long long int and returns a value of type long long.
+// The function uses a recursive approach to calculate the inverse of r modulo mod, which is a global variable defined at the beginning of the code with the value 1000000007.
+// The inverse of a number a modulo m is the number b such that (a * b) % m = 1. In this case, the function is trying to find the value of b such that (r * b) % mod = 1.
+// The function uses a recursive approach to solve this equation, starting with the base case r == 1, in which it returns 1. If r is not 1, the function returns mod - ((mod / r) * inv(mod % r)) % mod + mod) % mod, which is a recursive call to inv with the argument mod % r.
+// This recursive approach is based on the extended Euclidean algorithm, which is a method for finding the greatest common divisor of two numbers and can also be used to find the inverse of a number modulo m if a and m are relatively prime.
+// Keep in mind that this function assumes that r and mod are relatively prime. If they are not, the inverse will not exist and the function will not return the correct result.
 class Solution{
 	public:
 	     int mod = 1000000007;
