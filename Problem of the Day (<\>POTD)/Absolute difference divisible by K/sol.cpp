a6c78dce63c4a62475312bd06112caec9c8745e6
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-01-08 13:27:35 +0530

    Create sol.cpp

---
+++ Problem of the Day (<\>POTD)/Absolute difference divisible by K/sol.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    long long countPairs(int n, int arr[], int k) {
+        // code here
+        long long ans = 0;
+        int mp[k+1] = {0};
+        
+        for(int i=0;i<n;i++){  
+            ans += mp[arr[i]%k];
+            mp[arr[i]%k]++;
+        }
+        return ans;
+    }
+};
