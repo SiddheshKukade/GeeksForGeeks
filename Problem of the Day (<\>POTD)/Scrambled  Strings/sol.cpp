007d963f931b27fb41e9e580457686549c6aca19
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-01-29 00:44:17 +0530

    Add https://practice.geeksforgeeks.org/problems/scrambled-string/1

--- Problem of the Day (<\>POTD)/Scrambled  Strings/sol.cpp
+++ Problem of the Day (<\>POTD)/Scrambled  Strings/sol.cpp
@@ -1,38 +1,6 @@
 
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-
-class Geeks
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out=new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine().trim());
-
-        while(t-- > 0)
-        {
-            String s[]=in.readLine().trim().split(" ");
-            String a = s[0];
-            String b = s[1];
-            Solution g = new Solution();
-            if(g.isScramble(a,b)){
-                out.println("Yes");
-            }
-            else{
-                out.println("No");
-            }
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-class Solution{
+ class Solution{
+     
   static  HashMap<String, Boolean> map;
     static boolean isScramble(String S1,String S2)
     {
@@ -43,38 +11,41 @@ class Solution{
 
     static boolean fun(String S1, String S2)
     {
-        int n = S1.length();
+        int n = S1.length(); // length of first stirng both will hae same length by default.
         if(S1.equals(S2))
         {
-            map.put(S1+S2 , true);
+            map.put(S1+S2 , true); // storing that combo that is already true so that we don't have to compute the same thing again
             return true;
         }
-        if(map.containsKey(S1+S2))
+        if(map.containsKey(S1+S2)) // check if the combo is already computer
         {
-            return map.get(S1+S2);
+            return map.get(S1+S2); // if yes the get it's answer insted of chekcing again.
         }
 
         for(int i=1; i<n; i++)
         {
-            //1st case
+            //1st case Normal order
+            //breaking string one by one and swapping it with different combinations to check if it matches with each other.
             String a = S1.substring(0,i);
             String b = S2.substring(0,i);
+            
             String c = S1.substring(i,n);
             String d = S2.substring(i,n);
             if(fun(a,b) && fun(c,d))
             {
-                map.put(S1+S2, true);
+                map.put(S1+S2, true); // using memo as map storing the result
                 return true;
             }
 
             // 2nd case maybe in reverse order
             String e = S1.substring(0,i);
             String f = S2.substring(n-i,n);
+            
             String g = S1.substring(i,n);
             String h = S2.substring(0,n-i);
             if(fun(e,f) && fun(g,h))
             {
-                map.put(S1+S2, true);
+                map.put(S1+S2, true); // using memo as map storing the result
                 return true;
             }
 
