3588bcf1238ac5ffef2ff39fccaec8e499c89395
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-01-31 15:41:44 +0530

    Create sol.cpp

---
+++ Problem of the Day (<\>POTD)/Minimum times a has to be repeated such that B is substring of it/sol.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int minRepeats(string A, string B) {
+        // code here
+        if(B.size()==0) return 0;
+        string s = A;
+        int moves =1;
+        while(A.size()<B.size()){
+            A+=s;
+            moves++;
+        }
+        if(A.find(B) != -1){
+         return moves;   
+        }
+        A +=s;
+        moves++;
+        if(A.find(B) != -1){
+         return moves;   
+        }
+        return -1;
+    }
+};
