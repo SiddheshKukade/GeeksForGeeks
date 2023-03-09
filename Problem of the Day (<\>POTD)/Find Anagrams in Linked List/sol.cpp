ed347d1ff0296538f46aa24af9ad47782e6375bd
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-03-09 17:57:04 +0530

    Create sol.cpp

---
+++ Problem of the Day (<\>POTD)/Find Anagrams in Linked List/sol.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    vector<Node*> findAnagrams(struct Node* head, string s) {
+        vector<Node*> ans;
+        Node* start=head,*end=head;
+        int n=s.length();
+        
+        // pattern string
+        unordered_map<char,int> mp1;
+         // storing all occurences of string with their frequeiencies
+        for(auto it:s) mp1[it]++;
+        int count1=mp1.size();
+        
+        // traversing string
+        unordered_map<char,int> mp2;//second map to check the anagram
+        int count2=0;
+        while(end!=NULL)
+        {
+            mp2[end->data]++;
+            if(mp1[end->data]==mp2[end->data]) count2++;
+            
+            while(mp2[end->data]>mp1[end->data])
+            {
+                if(mp2[start->data]==mp1[start->data]) count2--;
+                mp2[start->data]--;
+                start=start->next;
+            }
+        
+            if(count2==count1)
+            {
+                ans.push_back(start);
+                Node* ahead=end->next;
+                end->next=NULL;
+                start=ahead,end=ahead;
+                mp2.clear();
+                count2=0;
+            }
+            else
+                end=end->next;
+        }
+        return ans;
+    }
+};
