0298ed3e684cca9febb34502a08b041759854e02
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-06-29 11:33:59 +0530

    Create sol.cpp

---
+++ Problem of the Day (<\>POTD)/Next Happy Number/sol.cpp
@@ -0,0 +1,37 @@
+Next Happy Numberclass Solution{
+public:
+    int isHappy(int N ){
+        if(N == 1 || N ==7){
+            return 1;
+        }
+        int sum = N, x= N;
+        while(sum > 9 ){
+            sum =0; 
+            while(x>0){
+                int d = x%10;
+                sum += d*d;
+                x/=10;
+            }
+            if(sum == 1){
+                return 1;
+            }
+            x = sum ; 
+        }
+         if(sum == 7){
+                return 1;
+            }
+            return 0;
+    }
+    int nextHappy(int N){
+        // code here
+  int x= N+1;
+  int res = x;
+      while(1){
+          if(isHappy(x)){
+              return x;
+          }
+          x++;
+          res =x;
+      }
+   return res;
+    }
